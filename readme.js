4407e97787e66148a2b1192dee5a3c7df3a897cb
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-02-15 15:34:41 +0100

    Mise à jour du fichier README.md et readme.js

--- readme.js
+++ readme.js
@@ -4,24 +4,36 @@ module.exports = `
 
 🚀 Passionné par le développement web, je suis actuellement en formation chez Le Wagon pour devenir développeur full stack. Fort d'une expérience antérieure en tant que chef de projet IT, j'ai décidé de changer de cap pour embrasser pleinement le monde du développement. Mon objectif est d'acquérir une expertise solide dans l'écosystème JavaScript, en me concentrant sur React, TypeScript, Node.js, et plus encore.
 
+---
+
 ### 🌱 Ce que j'apprends actuellement
 
 JavaScript: Découverte des fondamentaux et des concepts avancés pour des applications interactives.
+
 React: Immersion dans la création d'interfaces utilisateur dynamiques et réactives.
+
 TypeScript: Apprentissage de ce langage pour renforcer la qualité et la maintenabilité du code.
+
 Node.js: Exploration de l'environnement serveur pour développer des applications web complètes.
+
+---
 
 ### 🎯 Mes intérêts
 
 Développement Full Stack: Aspirer à devenir un développeur versatile capable de travailler à la fois sur le front-end et le back-end.
+
 Ecosystème JavaScript: Se spécialiser dans l'écosystème JS pour créer des applications modernes et performantes.
+
 Apprentissage Continu: Reste à l'affût des dernières technologies et meilleures pratiques en développement web.
+
+---
 
 ### 📫 Me contacter
 
 - [Mail](noureddine.benkerroum@gmail.com)
 - [Linkedin](https://www.linkedin.com/in/nbenkerroum/)
 
+---
 
 ### Visitor count
 
