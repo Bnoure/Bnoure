2dc4d79322b796121fa29063939ac28f3e83c5c9
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-02-15 15:19:33 +0100

    Mise à jour du fichier readme.js avec une introduction en français

--- readme.js
+++ readme.js
@@ -1,4 +1,6 @@
-module.exports = `### 👋 Salut, je suis Nour-Eddine Benkerroum
+module.exports = `
+
+### 👋 Salut, je suis Nour-Eddine Benkerroum
 
 🚀 Passionné par le développement web, je suis actuellement en formation chez Le Wagon pour devenir développeur full stack. Fort d'une expérience antérieure en tant que chef de projet IT, j'ai décidé de changer de cap pour embrasser pleinement le monde du développement. Mon objectif est d'acquérir une expertise solide dans l'écosystème JavaScript, en me concentrant sur React, TypeScript, Node.js, et plus encore.
 
