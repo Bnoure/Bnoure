7d3b4c8f0c938fad6f7092c5d960eccd2a46a513
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-02-15 17:56:23 +0100

    Mise à jour du fichier readme.js

--- readme.js
+++ readme.js
@@ -1,49 +1,27 @@
 module.exports = `
 
-### 👋 Salut, je suis Nour-Eddine Benkerroum
-
-🚀 Passionné par le développement web, je suis actuellement en formation chez Le Wagon pour devenir développeur full stack. Fort d'une expérience antérieure en tant que chef de projet IT, j'ai décidé de changer de cap pour embrasser pleinement le monde du développement. Mon objectif est d'acquérir une expertise solide dans l'écosystème JavaScript, en me concentrant sur React, TypeScript, Node.js, et plus encore.
-
----
-
-### 🌱 Ce que j'apprends actuellement
-
-JavaScript: Découverte des fondamentaux et des concepts avancés pour des applications interactives.
-
-React: Immersion dans la création d'interfaces utilisateur dynamiques et réactives.
-
-TypeScript: Apprentissage de ce langage pour renforcer la qualité et la maintenabilité du code.
-
-Node.js: Exploration de l'environnement serveur pour développer des applications web complètes.
-
----
-
-### 🎯 Mes intérêts
-
-Développement Full Stack: Aspirer à devenir un développeur versatile capable de travailler à la fois sur le front-end et le back-end.
-
-Ecosystème JavaScript: Se spécialiser dans l'écosystème JS pour créer des applications modernes et performantes.
-
-Apprentissage Continu: Reste à l'affût des dernières technologies et meilleures pratiques en développement web.
-
----
-
-### 📫 Me contacter
-
-- [Mail](noureddine.benkerroum@gmail.com)
-- [Linkedin](https://www.linkedin.com/in/nbenkerroum/)
-
----
-
-### Visitor count
-
-<img src="https://profile-counter.glitch.me/BNoure/count.svg" />
-
-Added the \`Mon 11 Sept 2023\`
-
-Last update on <#today_date>
-
-<#day_before_new_years> days before new years
-
-<#gabot_signing>
+### 👋 Salut, je suis Nour-Eddine Benkerroum\n
+🚀 Passionné par le développement web, je suis actuellement en formation chez Le Wagon pour devenir développeur full stack. Fort d'une expérience antérieure en tant que chef de projet IT, j'ai décidé de changer de cap pour embrasser pleinement le monde du développement. Mon objectif est d'acquérir une expertise solide dans l'écosystème JavaScript, en me concentrant sur React, TypeScript, Node.js, et plus encore.\n
+---\n
+### 🌱 Ce que j'apprends actuellement\n
+JavaScript: Découverte des fondamentaux et des concepts avancés pour des applications interactives.\n
+React: Immersion dans la création d'interfaces utilisateur dynamiques et réactives.\n
+TypeScript: Apprentissage de ce langage pour renforcer la qualité et la maintenabilité du code.\n
+Node.js: Exploration de l'environnement serveur pour développer des applications web complètes.\n
+---\n
+### 🎯 Mes intérêts\n
+Développement Full Stack: Aspirer à devenir un développeur versatile capable de travailler à la fois sur le front-end et le back-end.\n
+Ecosystème JavaScript: Se spécialiser dans l'écosystème JS pour créer des applications modernes et performantes.\n
+Apprentissage Continu: Reste à l'affût des dernières technologies et meilleures pratiques en développement web.\n
+---\n
+### 📫 Me contacter\n
+- [Mail](noureddine.benkerroum@gmail.com)\n
+- [Linkedin](https://www.linkedin.com/in/nbenkerroum/)\n
+---\n
+### Visitor count\n
+<img src="https://profile-counter.glitch.me/BNoure/count.svg" />\n
+Added the \`Mon 11 Sept 2023\`\n
+Last update on <#today_date>\n
+<#day_before_new_years> days before new years\n
+<#gabot_signing>\n
 `;
