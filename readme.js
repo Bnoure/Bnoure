e91254afaf5bd7cb3d24d3a481f25949f1791c9b
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-04-09 12:13:58 +0200

    Mise à jour du fichier readme.js avec une balise h1 pour le titre principal.

--- readme.js
+++ readme.js
@@ -1,9 +1,11 @@
 module.exports = `
 
 
+<div align="center">
 [![Typing SVG](https://readme-typing-svg.herokuapp.com?font=Fira+Code&duration=2000&pause=700&random=false&width=435&lines=Bienvenue++%F0%9F%91%8B+!!+;Je+suis+Nour-Eddine+Benkerroum+!++)](https://git.io/typing-svg)
+</div>
 
-### 👋 Hey, je suis Nour-Eddine Benkerroum\n
+
 
 
 🚀 Passionné par le développement web, Je poursuis ma passion pour le développement web après avoir complété ma formation chez Le Wagon, me préparant à embrasser pleinement la carrière de développeur full stack. Fort d'une expérience antérieure en tant que chef de projet IT, j'ai décidé de changer de cap pour embrasser pleinement le monde du développement. Mon objectif est d'acquérir une expertise solide dans l'écosystème JavaScript, en me concentrant sur React, TypeScript, Node.js, et plus encore.\n
